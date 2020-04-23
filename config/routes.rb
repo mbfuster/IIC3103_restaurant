e0b2d9f6ea9ef588cbcb0fae3e67bac09233d663
Belen Fuster <mbfuster@uc.cl>
2020-04-23 04:11:53 -0400

    nested resources incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :ingredients, path: :ingrediente
-  resources :burgers, path: :hamburguesa
+  resources :burgers, path: :hamburguesa do
+    resources :ingredients, path: :ingrediente
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
