09c7e96b4e194d9975c37bb385622a20fc038736
Belen Fuster <mbfuster@uc.cl>
2020-04-27 22:42:41 -0400

    fixed html error

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,7 +1,7 @@
 class Burger < ApplicationRecord
-  has_many :ingredient_on_burgers
   validates :name, presence: true
   validates :price, presence: true
   validates :description, presence: true
+  has_many :ingredient_on_burgers
   has_many :ingredients, through: :ingredient_on_burgers
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :ingredients, path: :ingrediente
+  resources :ingredients, path: :ingrediente, defaults: { format: :json }
 
-  resources :burgers, path: :hamburguesa do
-    resources :ingredients, path: :ingrediente, only: [:update, :destroy]
+  resources :burgers, path: :hamburguesa, defaults: { format: :json }do
+    resources :ingredients, path: :ingrediente, only: [:update, :destroy],defaults: { format: :json }
 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
