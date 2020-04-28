5b0e41d4e1055a8e315fa74dbebfffc9f724d35e
Belen Fuster <mbfuster@uc.cl>
2020-04-27 23:11:23 -0400

    added numeral validation

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,6 +1,6 @@
 class Burger < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true
   has_many :ingredient_on_burgers
   has_many :ingredients, through: :ingredient_on_burgers
